07f199615d89cc645772c489f74193ce46a83d6f
sanyamgoyal123 <42338275+sanyamgoyal123@users.noreply.github.com>
2020-07-19 12:03:01 +0530

    Update clothshopping.sql

--- clothshopping.sql
+++ clothshopping.sql
@@ -176,7 +176,12 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `active`, `address`, `email`, `name`, `password`, `phone`, `role`) VALUES
 (1, 1, 'Ram Bagh road', 'sanyamgoyal42@gmail.com', 'Sanyam Goyal', '$2a$10$weHjY7mdbSF8LVlPqHRSIee57jHEUucNUnaCQGothHa2.IJYC7O9.', '8219550861', 'ROLE_CUSTOMER');
-
+INSERT INTO `users` (`id`, `active`, `address`, `email`, `name`, `password`, `phone`, `role`) VALUES
+(2147483641, 1, '3200 West Road', 'customer1@email.com', 'customer1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', '123456789', 'ROLE_CUSTOMER'),
+(2147483642, 1, '2000 John Road', 'manager1@email.com', 'manager1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', '987654321', 'ROLE_MANAGER'),
+(2147483643, 1, '222 East Drive ', 'employee1@email.com', 'employee1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', '123123122', 'ROLE_EMPLOYEE'),
+(2147483645, 1, '3100 Western Road A', 'customer2@email.com', 'customer2', '$2a$10$0oho5eUbDqKrLH026A2YXuCGnpq07xJpuG/Qu.PYb1VCvi2VMXWNi', '2343456', 'ROLE_CUSTOMER'),
+(1, 1, 'sector46 c', 'joamsa2010@gmail.com', 'Pradeep Joam', '$2a$10$OL8acuIAyT0KJ7O3HwfS9uNcS48P/.rQfUbZ0T7RGBhJQ1CKrwTJW', '8195948484', 'ROLE_CUSTOMER');
 --
 -- Indexes for dumped tables
 --
